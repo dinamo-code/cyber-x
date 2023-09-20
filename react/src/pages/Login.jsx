031c48e892c695041e345291931c51b069d2d806
7assnain <m.7assnainprogramer@gmail.com>
2023-09-20 10:03:21 +0200

    login

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,7 +1,46 @@
-
+import cyberpurplelogo2 from './assets/cyberpurplelogo2.png'
+import verified2 from './assets/verified2.png'
 function Login() {
   return (
-    <div>Login</div>
+        <div id="signupcontainer">
+    <div id='mainsignup'>
+        <div id="signuphead">
+            <div id="logosignupcontainer">
+              <img src={cyberpurplelogo2} alt="cyberpurplelogo2"className='w-[45px] h-[45px]' />
+            </div>
+            <table id='signupheadlinks'>
+                <ul>
+                    <li><a href="/" title='Home page' id='signuphomehead'>Home</a></li>
+                    <li><a href="signup" title='signup page' id='loginheadbutton'>Sign up</a></li>
+                </ul>
+            </table>
+        </div>
+        <div id="signupcard">
+            <div id="signupcardverifiedimg">
+            <img src={verified2} alt="verified" id='verifiedssignupcard' className='w-[45px] h-[45px]' />
+            <h2 id='createacounttitle'>Login Account <span>+</span></h2>
+        </div>
+        <div id="signupfields">
+        <div id="signupname">
+        <h3>Name</h3>
+        <input type="text" placeholder='Enter your name' id='namefieled' required/>
+        </div>
+        <div id="signupemail">
+        <h3>Email</h3>
+        <input type="email" placeholder='Enter your Email' id='emailfieled' required/>
+        </div>
+        <div id="signuppassword">
+        <h3>Password</h3>
+        <input type="password" placeholder='Enter your Password' id='passwordfieled' required/>
+        </div>
+        </div>
+         <div id="loginsendbuttom">
+        <input type="button" value="Login" id='submitbtnlogin' />
+        </div>
+
+        </div>
+    </div>
+    </div>
   )
 }
 
